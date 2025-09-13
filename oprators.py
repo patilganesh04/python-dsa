09687ee12f3c9c2a59e312e90d18f0aea3fa7e44
Ganesh Patil <patilganesh935363@gmail.com>
2025-09-14 00:42:34 +0530

    pattern

--- oprators.py
+++ oprators.py
@@ -1,53 +1,82 @@
-#arithmetic
+# #arithmetic
 
-num1=10
-num2=20
-print(num1+num2) #=>30
+# num1=10
+# num2=20
+# print(num1+num2) #=>30
 
-print(num1-num2) #=>-10
+# print(num1-num2) #=>-10
 
-print(num1*num2) #=>200
+# print(num1*num2) #=>200
 
-print(num2/num1) #=>2.0
+# print(num2/num1) #=>2.0
 
-print(num2//num1) #=>2.0
+# print(num2//num1) #=>2.0
 
-nan=float('nan')
-print(nan)
+# nan=float('nan')
+# print(nan)
 
-pos_inf=float('inf')
-print(pos_inf)
+# pos_inf=float('inf')
+# print(pos_inf)
 
-neg_inf=float('-inf')
-print(neg_inf)
+# neg_inf=float('-inf')
+# print(neg_inf)
 
-#comparison oprators
+# #comparison oprators
 
-print(num1==num2)
-print(num1!=num2)
-print(num1<=num2)
+# print(num1==num2)
+# print(num1!=num2)
+# print(num1<=num2)
 
 
-#assignment oprators
+# #assignment oprators
 
-name="ganesh"
-x=5
+# name="ganesh"
+# x=5
 
-x=x+5
-print(x)
-x+=5
-print(x)
-x-=3
-print(x)
+# x=x+5
+# print(x)
+# x+=5
+# print(x)
+# x-=3
+# print(x)
 
-#logical
+# #logical
 
-print(True and True)
-print(True and False)
-print(False and False)
+# print(True and True)
+# print(True and False)
+# print(False and False)
 
-print(True or True)
-print(False or False)
-print(True or False)
+# print(True or True)
+# print(False or False)
+# print(True or False)
 
-print(16^2)
+# print(16^2)
+
+a = float("-inf")
+b = float("inf")
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# a=144
+# print(a>>1,a>>2,a>>3,a>>4,a>>5)
+
+# a=9
+# print(a<<1,a<<2,a<<3,a<<4,a<<5)
+a,b=14,6
+print((a>>1)+3*(b)-36/12)
+
+
+f=[]
+n=int(input("enter a number"))
+for i in range(n):
+    val=int(input("enter elment:"))
+    f.append(val)
+print(f)
+print(max(f))
+print(min(f))
+f.sort()
+print(f)
---
+++ pattern_practice.py
@@ -0,0 +1,32 @@
+# n = 5
+# for i in range(1, n+1):
+#     print("*" * i)
+
+# n=int(input("Enter number:"))
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1): 
+#         print("*",end=" ")
+#     print()
+#output:
+# * 
+# * *
+# * * *
+# * * * *
+
+# n=int(input("Enter number:"))
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print("*",end=" ")
+#     print()
+#output
+# * * * * 
+# * * * *
+# * * * *
+# * * * *
+n=int(input("Enter number:"))
+for i in range(n,0,-1):
+    for j in range(0,i): 
+        print("*",end=" ")
+    print()
